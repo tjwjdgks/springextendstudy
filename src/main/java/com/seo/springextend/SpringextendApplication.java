48bfe0a88cbb2563036dba102ae2c6192d3d606f
tjwjdgks <tjwjdgks43@gmail.com>
2021-08-16 12:43:44 +0900

    study extention

---
+++ src/main/java/com/seo/springextend/SpringextendApplication.java
@@ -0,0 +1,13 @@
+package com.seo.springextend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringextendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringextendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seo/springextend/SpringextendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seo.springextend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringextendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
